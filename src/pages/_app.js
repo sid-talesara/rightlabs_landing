b7768be6cbb0a6ee26eb1df4e3a80d4f6570ce68
imarajat24 <imarajatgulati@gmail.com>
2023-03-08 17:55:26 +0530

    Added variable height

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,17 @@
 import Head from "next/head";
 import Script from "next/script";
+import { useEffect } from "react";
 import "../../styles/style.scss";
 
 export default function App({ Component, pageProps }) {
+  // ! --------------------- Taking Variable height on client's device  ----------------------------------------
+
+  useEffect(() => {
+    let vh = window.innerHeight;
+    document.documentElement.style.setProperty("--height", `${vh}px`);
+    getComputedStyle(document.documentElement).getPropertyValue("--height");
+  }, []);
+
   return (
     <>
       <Script
